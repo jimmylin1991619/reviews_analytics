27d43e51c4b8c8b15b24384c3f4cf29100a2c8c3
John Doejimmylin <jimm1991619@hotmail.com>
2020-05-28 22:17:22 +0800

    add read part2

--- amazon檔案讀取.py
+++ amazon檔案讀取.py
@@ -6,12 +6,13 @@ with open('reviews.txt', 'r') as f:
 		count = count + 1
 		if count % 10000 == 0:  #%代表求餘數
 			print(len(data))
-print('檔案讀取玩了，總共有', len(data), '筆資料')
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-#print(data) 印出全部
-#print(data[0])
-#pirnt('------------------')
-#print(data[1])
+
+print(data) 印出全部
+print(data[0])
+pirnt('------------------')
+print(data[1])
 
 sum_len = 0
 for d in data:
@@ -21,8 +22,6 @@ print('留言的平均長度為', sum_len/len(data))
 
 
 # 清單做篩選
-
-
 new = []
 for d in data:
 	if len(d) < 100:
@@ -32,18 +31,55 @@ print(new[0])
 print(new[1])
 
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
-print('一共有', len(good), '筆留言有good')
-print(good[0])
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言有good')
+# print(good[0])
 
 
 new_good = [d for d in data if 'good' in d]  #上面的快寫法
 
 print('一共有', len(new_good), '筆留言有good')
 print(new_good[0])
+
+
+# bad = []
+# for d in data:
+# 	bad.append('bad' in d)
+# print(bad)
+
+
+bad = ['bad' in d for d in data]
+print(bad)
+
+
+#文字計數
+wc = {} # word_count
+for d in data:
+	words = d.strip().split() #預設值為空白鍵
+	for word in words:
+		if word in wc:
+			wc[word] += 1 #增加字的次數
+		else:
+			wc[word] = 1 #新增新的key進字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+print(wc['jimmy'])
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':	
+		break
+	if word in wc:
+		print(word, '出現過的次數為', wc[word])
+	else:
+		print('沒有這個字!')
+print('感謝使用')
 
 
 
